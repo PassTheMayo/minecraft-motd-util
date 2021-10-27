a3dc847c51d0ef57c483611096fd5c339f5ae93c
Jacob Gunther <jacobgunther03+github@gmail.com>
2021-10-27 16:11:33 -0500

    Add support for ParseResult in clean()

--- src/clean.ts
+++ src/clean.ts
@@ -1,12 +1,16 @@
 import assert from 'assert';
-import { CleanOptions } from './types';
+import { CleanOptions, ParseResult } from './types';
 
-export const clean = (text: string, options?: CleanOptions): string => {
-    assert(typeof text === 'string', `Expected 'text' to be typeof 'string', received '${typeof text}'`);
+export const clean = (text: ParseResult | string, options?: CleanOptions): string => {
+    assert(typeof text === 'string' || Array.isArray(text), `Expected 'text' to be typeof 'string' or 'array', received '${typeof text}'`);
 
     options = Object.assign({
         formattingCharacter: '\u00A7'
     }, options);
 
-    return text.replace(new RegExp(`${options.formattingCharacter}[0-9a-gk-or]`, 'g'), '');
+    if (typeof text === 'string') {
+        return text.replace(new RegExp(`${options.formattingCharacter}[0-9a-gk-or]`, 'g'), '');
+    }
+
+    return text.map((item) => item.text).join('');
 }
